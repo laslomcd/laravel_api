d5d426e653cf69ddf394e309fc4c3e201ac743cd
ryan <laslomcd@gmail.com>
2024-03-13 11:03:05 -0400

    working on api

--- app/Http/Controllers/API/V1/TaskController.php
+++ app/Http/Controllers/API/V1/TaskController.php
@@ -18,20 +18,14 @@ class TaskController extends Controller
         return TaskResource::collection(Task::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTaskRequest $request)
     {
-
+        $task = Task::create($request->validated());
+        return TaskResource::make($task);
     }
 
     /**
@@ -63,6 +57,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
